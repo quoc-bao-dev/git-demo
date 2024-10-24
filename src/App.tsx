2b1323cc638aac4b5b0ee42fa438ba9263f1a3a0
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2024-10-24 14:29:27 +0700

    fea: add button 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 const App = () => {
-    return <div>App
-        <h1>Thay đổi</h1>
-        <button>submit</button>
-        <h2>Thay đổi 3</h2>
-        <button>button 2</button>
-    </div>;
-
+    return (
+        <div>
+            App
+            <h1>Thay đổi</h1>
+            <button>submit</button>
+            <h2>Thay đổi 3</h2>
+            <button>button 2</button>
+            <button>button 3</button>
+        </div>
+    );
 };
 
 export default App;
